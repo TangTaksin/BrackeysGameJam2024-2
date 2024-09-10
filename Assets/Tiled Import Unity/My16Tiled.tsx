8950e721d73cdc8754c96519d7c3ce5afcd7056f
Daggoot <taksin_tang51@hotmail.com>
2024-09-10 17:43:05 +0700

    Adds Super Tiled and Tiled-Set

---
+++ Assets/Tiled Import Unity/My16Tiled.tsx
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="My16Tiled" tilewidth="16" tileheight="16" tilecount="120" columns="12">
+ <image source="../Sprites/TileSets/MyTileset.png" width="192" height="160"/>
+ <tile id="74">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.07074" y="15.6831">
+    <polygon points="0,0 0.0724392,-13.7634 5.97623,-13.7272 5.94001,-12.6769 10.9021,-12.6769 10.8297,-1.73854 5.79513,-1.81098 5.90379,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="75">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.217317" y="2.89757">
+    <polygon points="0,0 4.8172,0.108659 4.70855,-1.88342 10.7934,-1.81098 10.8659,-0.0724392 15.6831,-0.0362196 15.6831,10.9745 10.7934,10.9745 10.9383,12.858 4.88964,12.9304 4.92586,11.0108 -0.144878,10.9745"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="76">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-1.15903" y="1.52122" width="11.8438" height="14.0532"/>
+  </objectgroup>
+ </tile>
+ <tile id="86">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.9983" y="15.8642">
+    <polygon points="0,0 0,-15.9366 5.83135,-15.9728 5.86757,-0.0362196"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="88">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.07074" y="0.108659" width="5.94001" height="15.8642"/>
+  </objectgroup>
+ </tile>
+ <tile id="98">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.03452" y="15.7555">
+    <polygon points="0,0 0,-15.7555 6.04867,-15.828 6.12111,-12.9304 10.9383,-12.9304 10.8659,-1.8472 6.08489,-1.91964 6.19355,0.181098"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="99">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.325976" y="3.22354" width="15.5382" height="10.5037"/>
+  </objectgroup>
+ </tile>
+ <tile id="100">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.253537" y="0.108659" width="10.9383" height="15.4658"/>
+  </objectgroup>
+ </tile>
+ <tile id="101">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.8642" y="-0.0362196">
+    <polygon points="0,0 0.108659,15.9004 -5.94001,15.8642 -5.86757,0.0724392"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
