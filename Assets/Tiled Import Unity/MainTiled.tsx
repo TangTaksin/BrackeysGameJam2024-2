8018d1cb4f3ee117d3081708e32caf0ea703ad58
Daggoot <taksin_tang51@hotmail.com>
2024-09-11 19:58:19 +0700

    Update rain splash

---
+++ Assets/Tiled Import Unity/MainTiled.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="MainTiled" tilewidth="16" tileheight="16" tilecount="456" columns="24">
+ <image source="../Sprites/TileSets/MyTileset.png" width="389" height="304"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="78" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="79" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="80" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="102" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="103" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="104" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="126" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="127" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="128" wangid="0,1,0,1,0,1,0,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
