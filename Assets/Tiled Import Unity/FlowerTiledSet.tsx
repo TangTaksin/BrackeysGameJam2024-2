bd82eca9355492988d0ecb0440a0c06c1956e940
Daggoot <taksin_tang51@hotmail.com>
2024-09-13 20:24:01 +0700

    Adds all flower prefabs update sprite box and icon

---
+++ Assets/Tiled Import Unity/FlowerTiledSet.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Flower" tilewidth="16" tileheight="16" tilecount="66" columns="11">
+ <image source="../Sprites/TileSets/FLOWER SPRITE.png" width="176" height="96"/>
+ <tile id="53">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.9745" y="12.2784" width="5.07074" height="3.58574"/>
+  </objectgroup>
+ </tile>
+ <tile id="54">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.144878" y="11.047" width="6.41087" height="4.8172"/>
+  </objectgroup>
+ </tile>
+</tileset>
--- Assets/Tiled Import Unity/MyTileset.tsx
+++ Assets/Tiled Import Unity/MyTileset.tsx
@@ -1,6 +1,69 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="MyTileset" tilewidth="16" tileheight="16" tilecount="120" columns="15">
  <image source="../Sprites/TileSets/MyTileset.png" width="240" height="128"/>
+ <tile id="75">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.97623" y="15.828">
+    <polygon points="0,0 0.0362196,-1.95586 9.92417,-1.91964 9.92417,-12.8217 0.0362196,-12.8217 -0.0362196,-14.8138 -5.90379,-14.7414 -5.90379,0.144878"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="76">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6.08489" y="15.7193">
+    <polygon points="0,0 0,-1.73854 9.88795,-1.81098 9.81551,-12.7493 -0.181098,-12.6406 -0.181098,-14.6689 -6.12111,-14.7776 -6.08489,0.144878"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="77">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.90379" y="15.6469">
+    <polygon points="0,0 -0.0724392,-14.7051 -5.97623,-14.5241 -5.90379,0.144878"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="90">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0.108659" y="15.9004">
+    <polygon points="0,0 -0.0724392,-15.9728 5.83135,-15.9004 5.86757,-0.144878"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="92">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.79513" y="15.7193">
+    <polygon points="0,0 0.289757,-15.8642 -5.72269,-15.6831 -5.75891,0.0362196"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="105">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6.08489" y="15.7555">
+    <polygon points="0,0 -0.0724392,-1.88342 9.77929,-1.88342 9.88795,-12.7855 -0.181098,-12.8217 -0.217317,-15.8642 -6.01245,-15.7917 -6.12111,0.108659"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="106">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="13.9083">
+    <polygon points="0,0 16.0091,0.0724392 15.9366,-11.047 0.108659,-10.9021"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="107">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.97623" y="15.8642">
+    <polygon points="0,0 0,-16.1539 -6.01245,-15.9004 -5.97623,0.0724392"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="108">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.07074" y="15.828">
+    <polygon points="0,0 -0.0362196,-15.7917 5.90379,-15.7917 5.90379,0.0724392"/>
+   </object>
+  </objectgroup>
+ </tile>
  <wangsets>
   <wangset name="GroundPath" type="mixed" tile="-1">
    <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
@@ -29,6 +92,17 @@
    <wangtile tileid="60" wangid="0,1,0,0,0,0,0,0"/>
    <wangtile tileid="61" wangid="1,1,0,0,0,0,0,1"/>
    <wangtile tileid="62" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="75" wangid="0,0,0,1,1,0,0,0"/>
+   <wangtile tileid="76" wangid="0,0,0,1,1,1,0,0"/>
+   <wangtile tileid="90" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="91" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="92" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="105" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="106" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="108" wangid="1,0,0,0,1,0,0,0"/>
   </wangset>
  </wangsets>
 </tileset>
